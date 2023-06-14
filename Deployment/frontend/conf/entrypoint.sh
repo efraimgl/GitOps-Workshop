d00e094cb17d5c099aabe7622cdaebe728867227
efraimgl <42464789+efraimgl@users.noreply.github.com>
2023-06-14 11:17:30 +0000

    fix entry2

--- Deployment/frontend/conf/entrypoint.sh
+++ Deployment/frontend/conf/entrypoint.sh
@@ -3,5 +3,5 @@
 set -eu
 
 echo "update Nginx config"
-envsubst '\${HOST1} \${HOST2} \${PORT}' < /usr/share/nginx/html/config/default.conf > /etc/nginx/nginx.confcd 
+envsubst '\${HOST1} \${HOST2} \${PORT}' < /usr/share/nginx/html/config/default.conf > /etc/nginx/nginx.conf 
 exec "$@"
